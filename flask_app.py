aa29bfbb3b2e3357ff599892d9eb9b85e2a7e5c7
ballfollower <g.klosinski@hotmail.com>
2020-10-23 17:08:29 +0000

    Add database connection

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,23 @@
-
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="ballfollower",
+    password="{+@Np)$K8l",
+    hostname="ballfollower.mysql.pythonanywhere-services.com",
+    databasename="ballfollower$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 comments = []
 
