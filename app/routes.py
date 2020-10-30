718be1caead6437172696b9583d2d4ed2a3a218d
Gregdeg <gregdeg@op.pl>
2020-10-30 23:53:12 +0100

    Make 2 templates with a common base

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,23 @@
 from flask import render_template
 from app import app
-from tree import Tree
+from .tree import Tree
 
 @app.route('/')
 @app.route('/index')
 def index():
-    verseTree = Tree("")
+    verseTree = Tree('',[
+        Tree("This poem", [
+            Tree("I am writing to you"),
+            Tree("is extraordinary")
+        ]),
+        Tree("I like", [
+            Tree("jogging"),
+            Tree("cycling")
+        ])
+    ])
 
-    return render_template('index.html')
+    return render_template('index.html', verses = verseTree)
+
+@app.route('/instruction')
+def instruction():
+    return render_template('instruction.html')
