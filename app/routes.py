7dd99dc15d3f60f0427634435c1d9015b8d2c047
Gregdeg <gregdeg@op.pl>
2020-10-30 01:00:21 +0100

    Use dummy tree in routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from flask import render_template
 from app import app
+from tree import Tree
 
 @app.route('/')
 @app.route('/index')
 def index():
+    verseTree = Tree("")
+
     return render_template('index.html')
