6f2c8619a58c972816a63934f4f34d6c1a171e5b
ballfollower <g.klosinski@hotmail.com>
2020-11-13 19:56:31 +0100

    Bind id's to verse div's

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, \
+    jsonify
 from app import app
 from app.tree import Tree
 from app.models import Verse
@@ -17,20 +18,34 @@ def index():
     #     ])
     # ])
 
-    firstVerses = Verse.query.filter(Verse.parent == None).all()
-
-    return render_template('index.html', verses = firstVerses)
+    return render_template('index.html')
 
 @app.route('/instruction')
 def instruction():
     return render_template('instruction.html')
 
-@app.route('/ajax/verseProvider', methods=['POST'])
-def provideVerses():
-    if request.method == 'POST':
-        # verseToReturn = Verse.query.get(4)
-    #     result = []
-    #     for i in shows_list:
-    #         result.append(i.serialize(['id', 'date', 'title']))
-        return "ajax response here!"#verseToReturn.text
-        # pass
+def jsonifyVerses(verses):
+    return jsonify({
+        "ids":[verse.id for verse in verses],
+        "texts":[verse.text for verse in verses]
+    })
+
+@app.route('/ajax/initialVersesProvider', methods=['POST'])
+def provideInitialVerses():
+    firstVerses = Verse.query.filter(Verse.parent == None).all()
+
+    return jsonifyVerses(firstVerses)
+
+@app.route('/ajax/existingVerseHandler', methods=['POST'])
+def processExistingVerse():
+    # verseToReturn = Verse.query.get(4)
+    receivedVerseId = request.json['id']
+
+    nextVerses = Verse.query.filter(Verse.parentId == receivedVerseId).all()
+
+    # print(nextVerses)
+        
+    return jsonify({
+        "ids":[verse.id for verse in nextVerses],
+        "texts":[verse.text for verse in nextVerses]
+    })
--- app/static/script.js
+++ app/static/script.js
@@ -1,38 +1,80 @@
-$(function(){
-    var verseIdentificators = [];
-
-    $("#searchInput").on('keyup', function(e){
-      if (e.key === 'Enter' || e.keyCode === 13) {
-        if($("#poemSoFar").length){
-          $("#poemSoFar").append("<br>");
-        }
-        $("#poemSoFar").append($(this).val());
-        //alert($(this).val());
+$(function () {
+  $("#searchInput").on('keyup', function (e) {
+    if (e.key === 'Enter' || e.keyCode === 13) {
+      if ($("#poemSoFar").length) {
+        $("#poemSoFar").append("<br>");
       }
+      $("#poemSoFar").append($(this).val());
+      //alert($(this).val());
+    }
 
-      var input = $("#searchInput");
-      var filter = input.val().toUpperCase();
+    var input = $("#searchInput");
+    var filter = input.val().toUpperCase();
 
-      var versesToChoose = $(".verseToChoose");
+    var versesToChoose = $(".verseToChoose");
 
-      for (var i = 0; i < versesToChoose.length; i++) {
-        var txtValue = versesToChoose[i].textContent || versesToChoose[i].innerText;
+    for (var i = 0; i < versesToChoose.length; i++) {
+      var txtValue = versesToChoose[i].textContent || versesToChoose[i].innerText;
 
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          versesToChoose[i].style.display = "";
-        } else {
-          versesToChoose[i].style.display = "none";
-        }
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        versesToChoose[i].style.display = "";
+      } else {
+        versesToChoose[i].style.display = "none";
       }
-    });
+    }
+  });
 
-    $(".verseToChoose").on('click', function(){
-      alert($(this).index());
-    });
+  acquireInitialVerses();
 });
 
-function postVerse(){
-    $.post("http://127.0.0.1:5000/ajax/verseProvider", {year: '2017', semester: 1}, function(result){
-        console.log(result);
-    });
+function acquireInitialVerses(){
+  $.ajax({
+    url: "http://127.0.0.1:5000/ajax/initialVersesProvider",
+    contentType: "application/json;charset=utf-8",
+    data: JSON.stringify({}),
+    dataType: "json",
+    type: 'POST',
+    success: function (response) {
+      const verses = response;
+
+      for(var i = 0; i<verses.ids.length; i++){
+        var verseDiv = $("<div class='verseToChoose'></div>");
+        verseDiv.append(verses.texts[i]);
+        verseDiv.data("id", verses.ids[i]);
+        verseDiv.on('click', function () {
+          // alert("");
+          // postVerse($(this).data('verseId'));
+          // alert($(this).data("id"));
+        });
+        $("#verseAdditionPanel").append(verseDiv);
+      }
+    },
+    error: function (error) {
+      console.log(error);
+    }
+  });
+}
+
+function postVerse(id) {
+  // let verseToPost = {
+  //   'id': id
+  // }
+
+  $.ajax({
+    url: "http://127.0.0.1:5000/ajax/verseProvider",
+    contentType: "application/json;charset=utf-8",
+    data: JSON.stringify({ 'id': id }),
+    dataType: "json",
+    type: 'POST',
+    success: function (response) {
+      console.log(response);
+    },
+    error: function (error) {
+      console.log(error);
+    }
+  });
+
+  // $.post("http://127.0.0.1:5000/ajax/verseProvider", { id: id }, function (result) {
+  //   console.log(result);
+  // });
 }
